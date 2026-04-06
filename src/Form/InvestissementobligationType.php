b843ca7148d4848fd0120e7dfdd918ba94b4a870
Moemen_Meddeb <mmeddeb090@gmail.com>
2026-04-07 00:28:23 +0200

    final version of 3 CRUDS

--- src/Form/InvestissementobligationType.php
+++ src/Form/InvestissementobligationType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Loan\Investissementobligation;
+use App\Repository\WalletRepository;
+use App\Repository\ObligationRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -12,50 +14,54 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InvestissementobligationType extends AbstractType
 {
+    private $walletRepository;
+    private $obligationRepository;
+
+    public function __construct(WalletRepository $walletRepository, ObligationRepository $obligationRepository)
+    {
+        $this->walletRepository = $walletRepository;
+        $this->obligationRepository = $obligationRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        // Get wallets from database for dropdown
-        $walletChoices = [
-            'Wallet #3 - France (4711.40 EUR)' => 3,
-            'Wallet #4 - Tunisie (228.50 TND)' => 4,
-            'Wallet #5 - Tunisie (2367.00 TND)' => 5,
-            'Wallet #17 - British (4900.00 GBP)' => 17,
-            'Wallet #25 - Tunisie (0.00 TND)' => 25,
-            'Wallet #30 - Tunisie (1440.00 TND)' => 30,
-            'Wallet #31 - Anguilla (49880.00 XCD)' => 31,
-            'Wallet #32 - Algeria (2000.00 DZD)' => 32,
-        ];
+        // Get all wallets from database (filter by user_id = 1 for now)
+        $wallets = $this->walletRepository->findBy(['utilisateur_id' => 1]);
+        $walletChoices = [];
+        foreach ($wallets as $wallet) {
+            $walletChoices[$wallet->getPays() . ' - ' . $wallet->getSolde() . ' ' . $wallet->getDevise()] = $wallet->getId();
+        }
 
-        // Get obligations from database for dropdown
-        $obligationChoices = [
-            'Safe Bond - 5.5% for 12 months' => 1,
-            'Legacy For - 0.13% for 240 months' => 2,
-            'kdkd - 0.5% for 10 months' => 3,
-        ];
+        // Get all obligations from database
+        $obligations = $this->obligationRepository->findAll();
+        $obligationChoices = [];
+        foreach ($obligations as $obligation) {
+            $obligationChoices[$obligation->getNom() . ' - ' . $obligation->getTauxInteret() . '% for ' . $obligation->getDuree() . ' months'] = $obligation->getIdObligation();
+        }
 
         $builder
             ->add('walletId', ChoiceType::class, [
-                'label' => 'Select Wallet',
+                'label' => false,
                 'choices' => $walletChoices,
                 'attr' => ['class' => 'form-control'],
                 'placeholder' => '-- Choose a wallet --'
             ])
             ->add('obligationId', ChoiceType::class, [
-                'label' => 'Select Obligation',
+                'label' => false,
                 'choices' => $obligationChoices,
                 'attr' => ['class' => 'form-control'],
                 'placeholder' => '-- Choose an obligation --'
             ])
             ->add('montantInvesti', NumberType::class, [
-                'label' => 'Investment Amount',
+                'label' => false,
                 'attr' => ['class' => 'form-control', 'placeholder' => 'Enter amount to invest', 'step' => '0.01']
             ])
             ->add('dateAchat', DateType::class, [
-                'label' => 'Purchase Date',
+                'label' => false,
                 'widget' => 'single_text',
                 'html5' => false,
                 'format' => 'dd/MM/yyyy',
-                'attr' => ['class' => 'form-control datepicker', 'placeholder' => 'dd/mm/yyyy']
+                'attr' => ['class' => 'form-control datepicker', 'placeholder' => 'Select date']
             ]);
     }
 
