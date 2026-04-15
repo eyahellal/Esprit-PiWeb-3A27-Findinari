05501250f57f66e24f321b022fa2e1c661ccbe29
eyahellal <eyahellal9@gmail.com>
2026-04-15 15:49:03 +0100

    sla

---
+++ src/Service/TicketSlaCalculator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+class TicketSlaCalculator
+{
+    public function calculateDeadline(string $priority, \DateTimeInterface $createdAt): \DateTime
+    {
+        $deadline = \DateTime::createFromInterface($createdAt);
+
+        switch ($priority) {
+
+            case 'High':
+                $deadline->modify('+24 hours');
+                break;
+
+            case 'Medium':
+                $deadline->modify('+48 hours');
+                break;
+
+            case 'Low':
+            default:
+                $deadline->modify('+72 hours');
+                break;
+        }
+
+        return $deadline;
+    }
+
+    public function isBreached(?\DateTimeInterface $deadline): bool
+    {
+        if (!$deadline) {
+            return false;
+        }
+
+        return new \DateTime() > \DateTime::createFromInterface($deadline);
+    }
+}
